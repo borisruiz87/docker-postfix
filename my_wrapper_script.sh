2338fcc5ae7cbc6e4ed116c4029cd7a429e01709
borisruiz87 <86937980+borisruiz87@users.noreply.github.com>
2022-07-26 17:48:42 -0400

    Update my_wrapper_script.sh

--- my_wrapper_script.sh
+++ my_wrapper_script.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
-  
-# turn on bash's job control
-set -m
-  
-# Start the primary process and put it in the background
+
+# Start the first process
 ./usr/sbin/postfix start-fg &
   
-# Start the helper process
+# Start the second process
 ./rsyslogd -n &
   
-# the my_helper_process might need to know how to wait on the
-# primary process to start before it does its work and returns
-    
-# now we bring the primary process back into the foreground
-# and leave it there
-fg %1
+# Wait for any process to exit
+wait -n
+  
+# Exit with status of process that exited first
+exit $?
