9da2e8bfe02516c823072350a5c4061492abb6db
borisruiz87 <86937980+borisruiz87@users.noreply.github.com>
2023-01-14 23:34:00 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 postmap /etc/postfix/domains/transport
 postmap /etc/postfix/domains/relay_domains
 postmap /etc/postfix/domains/virtual
-postalias /etc/postfix/aliase
+postalias /etc/postfix/aliases
 
 
 exec "$@"
